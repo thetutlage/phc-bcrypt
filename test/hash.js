e9db5f52300778e0218bc4703d58fc4d147c39cb
Harminder Virk <virk.officials@gmail.com>
2021-02-10 20:18:38 +0530

    style: fix linter errors

--- test/hash.js
+++ test/hash.js
@@ -2,22 +2,22 @@ const test = require('ava');
 const m = require('..');
 
 test("should throw an error if the 'rounds' option is not a number", async (t) => {
-  const err = await t.throwsAsync(m.hash('password', {rounds: 'rounds'}));
-  t.is(err.message, "The 'rounds' option must be an integer");
+  const error = await t.throwsAsync(m.hash('password', {rounds: 'rounds'}));
+  t.is(error.message, "The 'rounds' option must be an integer");
 });
 
 test("should throw an error if the 'rounds' option is out of range", async (t) => {
-  let err = await t.throwsAsync(m.hash('password', {rounds: -1}));
-  t.regex(err.message, /The 'rounds' option must be in the range/);
+  let error = await t.throwsAsync(m.hash('password', {rounds: -1}));
+  t.regex(error.message, /The 'rounds' option must be in the range/);
 
-  err = await t.throwsAsync(m.hash('password', {rounds: 32}));
-  t.regex(err.message, /The 'rounds' option must be in the range/);
+  error = await t.throwsAsync(m.hash('password', {rounds: 32}));
+  t.regex(error.message, /The 'rounds' option must be in the range/);
 });
 
 test("should throw an error if the 'saltSize' option is out of range", async (t) => {
-  let err = await t.throwsAsync(m.hash('password', {saltSize: -1}));
-  t.regex(err.message, /The 'saltSize' option must be in the range/);
+  let error = await t.throwsAsync(m.hash('password', {saltSize: -1}));
+  t.regex(error.message, /The 'saltSize' option must be in the range/);
 
-  err = await t.throwsAsync(m.hash('password', {saltSize: 1025}));
-  t.regex(err.message, /The 'saltSize' option must be in the range/);
+  error = await t.throwsAsync(m.hash('password', {saltSize: 1025}));
+  t.regex(error.message, /The 'saltSize' option must be in the range/);
 });
